6cba3a7470a7176bd67fc6e183cc9ddf148878da
João Slengman <99735480+Slengm4n@users.noreply.github.com>
2025-10-24 11:29:07 -0300

    Update add_cpf.php

--- app/views/users/add_cpf.php
+++ app/views/users/add_cpf.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adicionar CPF - Kolae</title>
+    <title>Kolae</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
